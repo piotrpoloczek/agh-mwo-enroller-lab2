d33317df54b9669f5fe4b1dba3241444022cafe0
PiotrPoloczek <piotr@PiotrPoloczeks-MacBook-Pro.local>
2025-04-27 11:03:59 +0200

    add get by id, post, put, delete

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -1,6 +1,7 @@
 package com.company.enroller.controllers;
 
 
+import com.company.enroller.model.Meeting;
 import com.company.enroller.persistence.MeetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -8,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/meetings")
@@ -21,4 +24,44 @@ public class MeetingRestController {
     public ResponseEntity<?> getMeetings() {
         return new ResponseEntity<>(meetingService.getAll(), HttpStatus.OK);
     }
+
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getMeeting(@PathVariable("id") long id) {
+        Meeting meeting = meetingService.findById(id);
+        if (meeting == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(meeting, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public ResponseEntity<?> addMeeting(@RequestBody Meeting meeting) {
+        meetingService.add(meeting);
+        return new ResponseEntity<>(meeting, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteMeeting(@PathVariable("id") long id) {
+        Meeting meeting = meetingService.findById(id);
+        if (meeting == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        meetingService.delete(meeting);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public ResponseEntity<?> updateMeeting(@PathVariable("id") long id, @RequestBody Meeting updatedMeeting) {
+        Meeting meeting = meetingService.findById(id);
+        if (meeting == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        meeting.setTitle(updatedMeeting.getTitle());
+        meeting.setDescription(updatedMeeting.getDescription());
+        meeting.setDate(updatedMeeting.getDate());
+        meetingService.update(meeting);
+        return new ResponseEntity<>(meeting, HttpStatus.OK);
+    }
+
 }
--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -22,4 +22,26 @@ public class MeetingService {
 		return query.list();
 	}
 
+	public Meeting findById(long id) {
+		return session.get(Meeting.class, id);
+	}
+
+	public void add(Meeting meeting) {
+		session.beginTransaction();
+		session.save(meeting);
+		session.getTransaction().commit();
+	}
+
+	public void delete(Meeting meeting) {
+		session.beginTransaction();
+		session.delete(meeting);
+		session.getTransaction().commit();
+	}
+
+	public void update(Meeting meeting) {
+		session.beginTransaction();
+		session.update(meeting);
+		session.getTransaction().commit();
+	}
+
 }
